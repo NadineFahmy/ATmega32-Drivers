5b71f42719288b2a7430093fcd96884b51f3efeb
Nadine Fahmy <125144049+NadineFahmy@users.noreply.github.com>
2023-10-20 11:11:53 -0500

    Add files via upload

---
+++ HAL/LED/LED_config.h
@@ -0,0 +1,13 @@
+/*
+ * LED_config.h
+ *
+ *  Created on: Oct 20, 2023
+ *      Author: yoga
+ */
+
+#ifndef LED_CONFIG_H_
+#define LED_CONFIG_H_
+
+
+
+#endif /* LED_CONFIG_H_ */
---
+++ HAL/LED/LED_private.h
@@ -0,0 +1,13 @@
+/*
+ * LED_private.h
+ *
+ *  Created on: Oct 20, 2023
+ *      Author: yoga
+ */
+
+#ifndef LED_PRIVATE_H_
+#define LED_PRIVATE_H_
+
+
+
+#endif /* LED_PRIVATE_H_ */
