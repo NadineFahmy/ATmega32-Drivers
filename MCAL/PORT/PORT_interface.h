52feceb7c69ff5c3d7803b2284032ae77ba2905f
Nadine Fahmy <125144049+NadineFahmy@users.noreply.github.com>
2023-10-29 09:51:10 -0500

    Add files via upload

---
+++ MCAL/PORT/PORT_interface.h
@@ -0,0 +1,13 @@
+/*
+ * PORT_interface.h
+ *
+ *  Created on: Oct 29, 2023
+ *      Author: yoga
+ */
+
+#ifndef PORT_INTERFACE_H_
+#define PORT_INTERFACE_H_
+
+void PORT_voidInit(void);
+
+#endif /* PORT_INTERFACE_H_ */
---
+++ MCAL/PORT/PORT_program.c
@@ -0,0 +1,26 @@
+/*
+ * PORT_program.c
+ *
+ *  Created on: Oct 29, 2023
+ *      Author: yoga
+ */
+
+
+#include "STD_TYPES.h"
+
+#include "PORT_config.h"
+#include "PORT_private.h"
+#include "PORT_interface.h"
+
+void PORT_voidInit(void)
+{
+	DDRA_Register=PORTA_DIR;
+	DDRB_Register=PORTB_DIR;
+	DDRC_Register=PORTC_DIR;
+	DDRD_Register=PORTD_DIR;
+
+	PORTA_Register=PORTA_INITIAL_VALUE;
+	PORTB_Register=PORTB_INITIAL_VALUE;
+	PORTC_Register=PORTC_INITIAL_VALUE;
+	PORTD_Register=PORTD_INITIAL_VALUE;
+}
